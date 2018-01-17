d6f2a8729ac368679ada1cc8d223b9a20b64db18
le <18634342425@163.com>
2018-01-17 21:01:21 +0800

    quickSort 改进

--- sortTest/QuickSort.py
+++ sortTest/QuickSort.py
@@ -28,8 +28,9 @@ def quick2(list, left, right):
     key = list[left]
     begin, end = left, right
     while begin < end:
-        # 一定先从右边开始找第一个小于key的数
-        while begin < end and key <= list[end]: end -= 1
+        # 一定先从右边开始找第一个小于key的数   因为循环外面是left和begin交换 所以必须保证左边始终满足条件
+        while begin < end and key < list[end]: end -= 1
+        # 判断条件带等于是因为 key是左边第一个取得 如果不带，key第一次就交换走了
         while begin < end and key >= list[begin]:begin += 1
         list[begin],list[end] = list[end],list[begin]
     list[left],list[begin] = list[begin],list[left]
@@ -38,17 +39,17 @@ def quick2(list, left, right):
     return list
 
 if __name__ == '__main__':
-    # import random
-    # for i in range(10000):
-    #     list = []
-    #     tmp = []
-    #     for i in range(100):
-    #         x = random.randint(0, 99)
-    #         list.append(x)
-    #         tmp.append(x)
-    #     # print list
-    #     tmp.sort()
-    #     list = quick2(list, 0, len(list) - 1)
-    #     print tmp == list
-    list = [6,1,5,4,8,3,9,12,51,11,15,14,13,25,69,47,56,74,26,78]
-    print quick2(list,0,len(list)-1)
+    import random
+    for i in range(10000):
+        list = []
+        tmp = []
+        for i in range(100):
+            x = random.randint(0, 99)
+            list.append(x)
+            tmp.append(x)
+        # print list
+        tmp.sort()
+        list = quick(list, 0, len(list) - 1)
+        print tmp == list
+    # list = [6,1,5,4,8,3,9,12,51,11,15,14,13,25,69,47,56,74,26,78]
+    # print quick(list,0,len(list)-1)
